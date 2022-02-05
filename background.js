1f593f6d2aae2f4adb8e99237eb18b4932a7196b
aleruiz <ales.email@gmail.com>
2022-02-05 15:26:10 -0500

    Working prototype

---
+++ background.js
@@ -0,0 +1,23 @@
+function onError(error) {
+  console.error(`Error: ${error}`);
+}
+
+function sendMessageToTabs(tabs) {
+  for (let tab of tabs) {
+    browser.tabs.sendMessage(
+      tab.id,
+      {message: 'bypass'}
+    ).then(response => {
+      console.log("Message from the content script:");
+      console.log(response.response);
+    }).catch(onError);
+  }
+}
+
+browser.browserAction.onClicked.addListener(function(){
+  console.log('Triggering extension');
+  browser.tabs.query({
+    currentWindow: true,
+    active: true
+  }).then(sendMessageToTabs).catch(onError);
+});
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function getBackgroundColor(el){
+  // A helper function for removeLockoutDiv. Returns an elements background-color.
   if (window.getComputedStyle){
     var y = document.defaultView.getComputedStyle(el, null).getPropertyValue(
       'background-color'
@@ -11,6 +12,10 @@ function getBackgroundColor(el){
 }
 
 function removeLockoutDiv() {
+  // Looks through divs to match the background-color with twitter's
+  // known lockout div's background color, then removes the div.
+  let twitterLockoutDivColor = "rgba(91, 112, 131, 0.4)";
+
   let divs = document.getElementsByTagName('div');
   for (var i in divs){
     let el = divs[i];
@@ -22,24 +27,27 @@ function removeLockoutDiv() {
       continue;
     }
     if (color == twitterLockoutDivColor){
+      console.log('Extension Log: removing potential lockout div');
       el.style.display = "none";
     }
   }
 }
 
-
 function bypassSoftPaywall(){
-  // add scroll
+  // add scroll back-in
   document.getElementsByTagName('html')[0].style.overflow = "scroll";
 
   // remove modal
   let els = document.querySelectorAll('[data-testid=sheetDialog]');
-  els[0].style.display = "none";
+  document.querySelectorAll('[data-testid=sheetDialog]')[0].style.display = "none"
 
   // remove lockout gray div
-  let twitterLockoutDivColor = "rgba(91, 112, 131, 0.4)";
   removeLockoutDiv();
+
 }
 
-
-browser.browserAction.onClicked.addListener(bypassSoftPaywall);
+browser.runtime.onMessage.addListener(function(request){
+  bypassSoftPaywall();
+  return Promise.resolve({'success': true})
+});
+console.log('Extension content script loaded');
